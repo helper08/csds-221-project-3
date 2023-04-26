aefc98d80f24c20159452ebd4740dbcae7fdc675
helper08 <ccpg02@gmail.com>
2023-04-26 01:08:07 -0400

    whatever

---
+++ build/server/shared/Environments.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Environments;
+(function (Environments) {
+    Environments["PRODUCTION"] = "production";
+    Environments["DEVELOPMENT"] = "development";
+})(Environments || (Environments = {}));
+exports.default = Environments;
+//# sourceMappingURL=Environments.js.map
