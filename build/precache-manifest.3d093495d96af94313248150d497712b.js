833f251671b98d193fa7282520a54cb51d95f56f
helper08 <ccpg02@gmail.com>
2023-04-26 00:45:41 -0400

    deploy

---
+++ build/precache-manifest.3d093495d96af94313248150d497712b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b426fd198a8fe95ac844",
+    "url": "/csds-221-project-3/static/css/main.1da94fb0.chunk.css"
+  },
+  {
+    "revision": "b426fd198a8fe95ac844",
+    "url": "/csds-221-project-3/static/js/main.b426fd19.chunk.js"
+  },
+  {
+    "revision": "89c209f75d6283bd0630",
+    "url": "/csds-221-project-3/static/css/1.2c5bdb61.chunk.css"
+  },
+  {
+    "revision": "89c209f75d6283bd0630",
+    "url": "/csds-221-project-3/static/js/1.89c209f7.chunk.js"
+  },
+  {
+    "revision": "5f279a54428ad6dfa903",
+    "url": "/csds-221-project-3/static/js/runtime~main.5f279a54.js"
+  },
+  {
+    "revision": "9b0fff603aef31552f0bc841feea4abb",
+    "url": "/csds-221-project-3/index.html"
+  }
+];
