8b22e474678f54a36692946a7f916406b51d38d7
helper08 <ccpg02@gmail.com>
2023-04-26 05:45:16 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,27 @@
 import ReactDOM from 'react-dom/client';
 import Root from './client/components/Root';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { StaticRouter } from 'react-router-dom/server';
 
 async function start() {
-    const root = ReactDOM.createRoot(
-      document.getElementById('root') as HTMLElement
-    );
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+    },
+  ]);
+
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
+
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
   
-    root.render(
-      <React.StrictMode>
-        <BrowserRouter>
-          <Root />
-        </BrowserRouter>
-      </React.StrictMode>
-    );
-  }
-  
-  start();
+start();
