c8c64a7b9bc1afff88b6215ce90da06e70f428d1
helper08 <ccpg02@gmail.com>
2023-04-26 02:08:19 -0400

    vhn

--- build/server/shared/Environments.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Environments;
-(function (Environments) {
-    Environments["PRODUCTION"] = "production";
-    Environments["DEVELOPMENT"] = "development";
-})(Environments || (Environments = {}));
-exports.default = Environments;
-//# sourceMappingURL=Environments.js.map
